d29b99694ea50039a6aa2c547f7bcc6288ccd456
Mehul Rathod <rathodmehul1011@gmail.com>
2022-12-28 14:58:27 +0530

    add rs symbol & 2023

--- expenses/src/components/Expenses/ExpenseItem.js
+++ expenses/src/components/Expenses/ExpenseItem.js
@@ -10,7 +10,7 @@ const ExpenseItem = (props) => {
         <ExpenceDate date={props.date} />
         <div className="expense-item__description">
           <h2>{props.title}</h2>
-          <div className="expense-item__price">${props.amount}</div>
+          <div className="expense-item__price">₹{props.amount}</div>
         </div>
       </Card>
     </li>
